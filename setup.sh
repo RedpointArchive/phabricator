8e753c8c86fba6cc040662bef72f0ad938c00d22
June Rhodes <jrhodes@redpointgames.com.au>
2016-12-05 13:15:28 +1100

    Prevent Git 2.11 from being installed
    Fixes #62.  This version of Git is broken as per https://secure.phabricator.com/T11940.

--- setup.sh
+++ setup.sh
@@ -6,10 +6,25 @@ set -x
 # Add repositories
 zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_42.1/ php
 zypper --non-interactive ar http://download.opensuse.org/repositories/home:/marec2000:/nodejs/openSUSE_Leap_42.1/ nodejs
+
+# Install Git before we add the SCM repository (the SCM repository contains Git 2.11, which is broken).
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution git
+
+# Lock the git package to the current version
+zypper --non-interactive al git
+
+# Test to make sure we're not running Git 2.11, otherwise, abort the image bake right now (this prevents
+# bad images from being pushed to the index).
+if [ "$(git --version)" == *"2.11"* ]; then
+  echo "Bad version of Git detected: $(git --version).  Aborting image creation!"
+  exit 1
+fi
+
+# Add SCM package for other tools (Subversion, Mercurial)...
 zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_Leap_42.1/ scm
 
 # Install requirements
-zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php-fpm php5-mbstring php5-mysql php5-curl php5-pcntl php5-gd php5-openssl php5-ldap php5-fileinfo php5-posix php5-json php5-iconv php5-ctype php5-zip php5-sockets which python-Pygments nodejs ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php5-xmlwriter nodejs-ws git php5-opcache ImageMagick postfix glibc-locale
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php-fpm php5-mbstring php5-mysql php5-curl php5-pcntl php5-gd php5-openssl php5-ldap php5-fileinfo php5-posix php5-json php5-iconv php5-ctype php5-zip php5-sockets which python-Pygments nodejs ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php5-xmlwriter nodejs-ws php5-opcache ImageMagick postfix glibc-locale
 
 # Build and install APCu
 zypper --non-interactive install --force-resolution autoconf automake binutils cpp cpp48 gcc gcc48 glibc-devel libasan0 libatomic1 libcloog-isl4 libgomp1 libisl10 libitm1 libltdl7 libmpc3 libmpfr4 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libstdc++48-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 make ncurses-devel pcre-devel php5-devel php5-pear php5-zlib pkg-config readline-devel tack xz-devel zlib-devel
